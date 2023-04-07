4e60616f86a4b86ee1107933b6532e71a2f943e0
StanislavPopovSP <staspopov4@gmail.com>
2023-04-07 22:29:00 +0500

    Добавил два класса для читаемого вида в Админке(BlogAdmin, CategoryAdmin).

--- programming_blog/blog/admin.py
+++ programming_blog/blog/admin.py
@@ -3,10 +3,17 @@ from .models import *
 
 class BlogAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('title',)} # именованный параметр. {} - какое поле будет переводить другое поле. Ключ slug и что бы данные брал из поля title(передавать данные надо в виде кортежа.) Надо что бы slug переводил те данные кот-е есть в title.
-
+    list_display = ('id', 'title', 'cat', 'time_created', 'photo', 'is_published') # список отображаемых элементов, в него передаем кортеж и передаем какие поля хотим видеть. Чаще всего используется id номер самого элемента.
+    list_display_links = ('id', 'title') # Чтобы была ссылка на выбранные поля
+    search_fields = ('title', 'content') # поиск по полям
+    list_editable = ('is_published',) # для того что бы поле было кликабельным, для выбора не заходя по ссылке.
+    list_filter = ('is_published', 'time_created') # фильтр полей
 
 class CategoryAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('name',)}
+    list_display = ('id', 'name')
+    list_display_links = ('id', 'name')
+    search_fields = ('name',)
 
 
 admin.site.register(Blog, BlogAdmin)
