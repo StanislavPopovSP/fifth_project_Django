14cb81e988696676aa0f8e54ac0d123ed9a3d9d6
StanislavPopovSP <staspopov4@gmail.com>
2023-04-07 22:26:13 +0500

    Добавил читаемое поле для всех модели

--- programming_blog/blog/models.py
+++ programming_blog/blog/models.py
@@ -1,15 +1,15 @@
 from django.db import models
 
 class Blog(models.Model):
-    title = models.CharField(max_length=255) # обязательное для заполнения
-    slug = models.SlugField(max_length=255, unique=True) # название транслитерации какой-то статьи, что-бы в адресной строке появлялось в место id если будем заходить на конкретную новость, либо запись, что бы в адресной строке прописывалось полностью название новости. Не может быть с одним и тем же названием.
-    content = models.TextField(blank=True) # контент статьи
-    photo = models.ImageField(upload_to='photos/%Y/%m/%d', blank=True) # Y-год, m - месяц, d - день
-    time_created = models.DateTimeField(auto_now_add=True) # дата создания какой-то записи
-    time_update = models.DateTimeField(auto_now=True) # фиксация даты обновления, будет фиксироваться автоматически, в момент когда мы будем создать какую-то запись.
+    title = models.CharField(max_length=255, verbose_name='Название') # обязательное для заполнения. verbose_name - относится к админке и отношения к БД не имеет.
+    slug = models.SlugField(max_length=255, unique=True, verbose_name='URL') # название транслитерации какой-то статьи, что-бы в адресной строке появлялось в место id если будем заходить на конкретную новость, либо запись, что бы в адресной строке прописывалось полностью название новости. Не может быть с одним и тем же названием.
+    content = models.TextField(blank=True, verbose_name='Контент') # контент статьи
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d', blank=True, verbose_name='Фото') # Y-год, m - месяц, d - день
+    time_created = models.DateTimeField(auto_now_add=True, verbose_name='Дата публикации') # дата создания какой-то записи
+    time_update = models.DateTimeField(auto_now=True, verbose_name='Дата обновления') # фиксация даты обновления, будет фиксироваться автоматически, в момент когда мы будем создать какую-то запись.
     # мы можем опубликовывать запись или создавать в той-же админке, а подготовить набор элементов и в нужный момент их публиковать.
-    is_published = models.BooleanField(default=True) # может быть опубликована, может нет. По умолчанию опубликована.
-    cat = models.ForeignKey('Category', on_delete=models.PROTECT) # категория. Так как класс ниже, имя класса указываем в кавычках. Запрещается удалять записи из первичной модели, если она используется во вторичной, выдает исключение.
+    is_published = models.BooleanField(default=True, verbose_name='Опубликовано') # может быть опубликована, может нет. По умолчанию опубликована.
+    cat = models.ForeignKey('Category', on_delete=models.PROTECT, verbose_name='Категория') # категория. Так как класс ниже, имя класса указываем в кавычках. Запрещается удалять записи из первичной модели, если она используется во вторичной, выдает исключение.
 
     def __str__(self):
         return self.title
@@ -21,8 +21,8 @@ class Blog(models.Model):
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=100) # название категории
-    slug = models.SlugField(max_length=200, unique=True)
+    name = models.CharField(max_length=100, verbose_name='Категория') # название категории
+    slug = models.SlugField(max_length=200, unique=True, verbose_name='URL')
 
     def __str__(self):
         return self.name
