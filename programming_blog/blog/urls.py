b322fce4a6cbde3a59af6ddf73c9e638ec17304f
StanislavPopovSP <staspopov4@gmail.com>
2023-04-07 16:00:29 +0500

    Исправил ошибки

--- programming_blog/blog/urls.py
+++ programming_blog/blog/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import * # так как работа ведется с классами, то импортируем всё что есть в документе.
 
 urlpatterns = [
-    path('', BlogHome.as_views(), name='index'), # as_views - привязать к маршруту. Т.е привязать текущий класс к текущему маршруту '' данного пути.
+    path('', BlogHome.as_view(), name='index'), # as_views - привязать к маршруту. Т.е привязать текущий класс к текущему маршруту '' данного пути.
 ]
