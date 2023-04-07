cb3ead72aeed2aa6690f0c82c5de029656cb7c3f
StanislavPopovSP <staspopov4@gmail.com>
2023-04-07 20:09:59 +0500

    Добавил в Админке возможность объединения двух полей.

--- programming_blog/blog/admin.py
+++ programming_blog/blog/admin.py
@@ -1,5 +1,13 @@
 from django.contrib import admin
 from .models import *
 
-admin.site.register(Blog)
-admin.site.register(Category)
+class BlogAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('title',)} # именованный параметр. {} - какое поле будет переводить другое поле. Ключ slug и что бы данные брал из поля title(передавать данные надо в виде кортежа.) Надо что бы slug переводил те данные кот-е есть в title.
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
+
+
+admin.site.register(Blog, BlogAdmin)
+admin.site.register(Category, CategoryAdmin)
