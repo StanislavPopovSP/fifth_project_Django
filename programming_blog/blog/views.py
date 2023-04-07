bec8327e5ffbea57860e650dfa429c8346fa42a1
StanislavPopovSP <staspopov4@gmail.com>
2023-04-07 12:29:54 +0500

    Добавил обработчик BlogHome для вывода данных на html шаблон

--- programming_blog/blog/views.py
+++ programming_blog/blog/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views.generic import ListView  # он за нас сделает большую работу, не надо будет указывать много элементов одновременно.
+from .models import *
 
-# Create your views here.
+
+# если работаем с классом, то без модели он работать не может
+class BlogHome(ListView):
+    model = Blog
+    template_name = 'blog/index.html' # куда будет выводиться HTML страница. template_name - как свойства в классе, они имеют определенное значение, что мы в них должны поместить.
+    context_object_name = 'post' # Имя объекта внутри шаблона. Специальная переменная которая будет выводить код в html шаблон.
