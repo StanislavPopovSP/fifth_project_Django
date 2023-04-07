a9e67303699f63f684f8723e08a420835c9ea168
StanislavPopovSP <staspopov4@gmail.com>
2023-04-07 18:23:13 +0500

    Добавил к классу метод get_context_data для вывода данных на html шаблон

--- programming_blog/blog/views.py
+++ programming_blog/blog/views.py
@@ -2,9 +2,19 @@ from django.shortcuts import render
 from django.views.generic import ListView  # он за нас сделает большую работу, не надо будет указывать много элементов одновременно.
 from .models import *
 
+menu = [
+    {'title': 'Добавить статью', 'url_name': 'index'}, # url_name - будет лежать путь, если будет пусто будет ошибка, т.к ни чего не существует.
+    {'title': 'Войти', 'url_name': 'index'} #
+] # добавим пункты меню
 
 # если работаем с классом, то без модели он работать не может
 class BlogHome(ListView):
     model = Blog
     template_name = 'blog/index.html' # куда будет выводиться HTML страница. template_name - как свойства в классе, они имеют определенное значение, что мы в них должны поместить.
     context_object_name = 'post' # Имя объекта внутри шаблона. Специальная переменная которая будет выводить код в html шаблон.
+
+    def get_context_data(self, *, object_list=None, **kwargs): # данный метод сущ-ет для любого класса который наследуется от ListView.
+        context = super().get_context_data(**kwargs) # контекст есть у родительского класса, получаем context для шаблона со всеми ключами и значениями.
+        context['title'] = 'Главная страница' # на главную страницу добавляем заголовок
+        context['menu'] = menu # Что бы их можно было увидеть # Когда обратимся к ключу 'menu' получим доступ к menu.
+        return context
