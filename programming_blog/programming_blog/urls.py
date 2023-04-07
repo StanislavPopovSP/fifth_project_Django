d78a52048f353e7d7c44e925484b68cbdee518c0
StanislavPopovSP <staspopov4@gmail.com>
2023-04-07 16:28:26 +0500

    Прописал пути для медиа файлов

--- programming_blog/programming_blog/urls.py
+++ programming_blog/programming_blog/urls.py
@@ -15,8 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')), # папка блог и urls.py
 ]
+
+if settings.DEBUG: # это отрабатывает на локальном хостинге, а когда будет находиться в интернете не будет работать.
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) # на хостинге данные настройки будут прописываться отдельно.
