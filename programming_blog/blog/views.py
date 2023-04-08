145d35498cf496ab7534a6ca7ade30550abbf339
Станислав <staspopov4@gmail.com>
2023-04-08 21:06:26 +0500

    Добавил обработчик для вывода статьи

--- programming_blog/blog/views.py
+++ programming_blog/blog/views.py
@@ -17,8 +17,14 @@ class BlogHome(ListView):
     def get_context_data(self, *, object_list=None, **kwargs): # данный метод сущ-ет для любого класса который наследуется от ListView.
         context = super().get_context_data(**kwargs) # контекст есть у родительского класса, получаем context для шаблона со всеми ключами и значениями.
         context['title'] = 'Главная страница' # на главную страницу добавляем заголовок
+        context['cat_selected'] = 0
         context['menu'] = menu # Что бы их можно было увидеть # Когда обратимся к ключу 'menu' получим доступ к menu.
         return context
+
+    def get_queryset(self): # Верните список элементов для этого представления. Возвращаемое значение должно быть итеративным и может быть экземпляром QuerySet
+        """Делаем ограничения"""
+        return Blog.objects.filter(is_published=True).select_related('cat') # если статья опубликована. Берем из БД Blog только те записи которые будут опубликованы. select_related - связали с категорией.
+
 
 class ShowPost(DetailView):
     """Будет просматривать отдельную статью"""
@@ -32,3 +38,11 @@ class ShowPost(DetailView):
         context['title'] = context['post'] # В каждой статье будет название свое конкретное. context[] - в контекст передадим post, что бы попадало имя конкретной статьи в title.
         context['menu'] = menu # Что бы их можно было увидеть # Когда обратимся к ключу 'menu' получим доступ к menu.
         return context
+
+class BlogCategory(ListView):
+    model = Blog  # с какой моделью работаем
+    template_name = 'blog/post.html'  # имя шаблона
+    slug_url_kwarg = 'post_slug'  # имя slug из пути
+    allow_empty = False # не разрешим выводить пустые категории
+    # есть статьи хот-е опубликованы либо нет
+
