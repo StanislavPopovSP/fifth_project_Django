5cf8474726478c23bed5954f6429f441915e5f83
StanislavPopovSP <staspopov4@gmail.com>
2023-04-30 22:47:55 +0500

    Добавил комментарии в blog_tags.py

--- programming_blog/blog/templatetags/blog_tags.py
+++ programming_blog/blog/templatetags/blog_tags.py
@@ -9,9 +9,23 @@ register = template.Library()
 
 
 @register.inclusion_tag('blog/list_categories.html')  # нужно указать что это включающий тег
-def show_categories(sort=None, cat_selected=0):  # два принимаемых аргумента, они имеют значения по умолчанию. Их создали сами.
+def show_categories(sort=None):  # два принимаемых аргумента, они имеют значения по умолчанию. Их создали сами.
     if not sort:  # если нет сортировки
         cats = Category.objects.all()
     else:
         cats = Category.objects.order_by(sort)
-    return {'cats': cats, 'cat_selected': cat_selected}  # если cat_selected заменится на другое значение, то будет оно, или по умолчанию.
+    return {'cats': cats}  # если cat_selected заменится на другое значение, то будет оно, или по умолчанию.
+
+
+
+## Как пример других забросов
+# @register.simple_tag() - забирает какую-то информацию от куда либо, он ее просто передает context в шаблон, где мы вызовем этот тег, и он вернет эту информацию.
+# def get_catigories():
+#     """Вывод всех категорий"""
+#     return Category.objects.all()
+
+
+# @register.inclusion_tag('movies/tags/last_movie.html') - рендерит шаблон, где мы вызовим этот temlate тег он вернет отрендариный html
+#     def get_last_movies(count=5):
+#         movie = Movie.objects.order_by('id')[:count]
+#         return {'last_movies': movie}
